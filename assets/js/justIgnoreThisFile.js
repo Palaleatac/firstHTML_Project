14aab469eb8caefd25abf91dba2d2e4f2abb835d
Palaleatac <palaleatac@gmail.com>
2026-05-12 21:42:12 +0700

    added reworked template

---
+++ assets/js/justIgnoreThisFile.js
@@ -0,0 +1,5 @@
+let convenientItemCrossSign=document.getElementById('convenientLinks');
+let convenientItemMenu=document.getElementById('convenientItems');
+convenientItemCrossSign.onclick=function(){
+    convenientItemMenu.classList.toggle('active');
+}
