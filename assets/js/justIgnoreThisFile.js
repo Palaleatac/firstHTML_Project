8e96b49eb422af5165ec53bb26348743407c9b67
Palaleatac <palaleatac@gmail.com>
2026-05-13 09:56:26 +0700

    modified feature/template

--- assets/js/justIgnoreThisFile.js
+++ assets/js/justIgnoreThisFile.js
@@ -1,5 +1,9 @@
 let convenientItemCrossSign=document.getElementById('convenientLinks');
 let convenientItemMenu=document.getElementById('convenientItems');
 convenientItemCrossSign.onclick=function(){
-    convenientItemMenu.classList.toggle('active');
+    convenientItemMenu.classList.toggle('active'); /*khi CrossSign được bấm, thì class của ItemMenu sẽ được thay đổi (thêm bớt 'tapThis')*/
 }
+/*Giải thích cơ chế:
+        khi bấm vào '+' thì thẻ <div id="ItemMenu"> sẽ được thêm class="tapThis"
+        Khi mà <div> đó là 'id="ItemMenu" class="tapThis"' thì sẽ chạy khối lệnh dưới, còn bộ chọn ở cuối thì tùy theo cách đặt thẻ nào sẽ được thêm/bớt class
+        Còn khi mà nó vẫn chỉ là 'id="ItemMenu" và không có class, thì nó sẽ chạy theo khối css không có class'*/
